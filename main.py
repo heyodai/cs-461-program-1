e4584eb8fa6d93454b0075eac079be3d0d39fc43
Odai Athamneh <heyodai@gmail.com>
2022-09-25 18:02:40 -0500

    working but inefficient build

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ while True:
 # find the shortest path
 path = []
 current_town = town_list[start]
+do_not_check = None
 
 # while (current_town.name != goal):
 while True:
@@ -26,22 +27,35 @@ while True:
         "distance": float("inf")
     }
     for town in frontier:
+        if (town in path or town == do_not_check):
+            # we've already been here
+            continue
+
         distance = town_list[town].distance(town_list[goal])
         if (distance < closest["distance"]):
             closest["name"] = town
             closest["distance"] = distance
 
-    # add it to the path
-    path.append(closest["name"])
-    current_town = town_list[closest["name"]]
+    if (closest["name"] is None):
+        # no more frontier towns
+        # time to backtrack
+
+        do_not_check = current_town.name
+        path.pop()
+        current_town = town_list[path[-1]]
+
+        print("Backtracking to", current_town.name)
+        continue
+    else:
+        # add it to the path
+        path.append(closest["name"])
+        current_town = town_list[closest["name"]]
 
     # if it's the goal, stop
     if (current_town.name == goal):
         print("Path found!")
         break
 
-    # if it's a dead end, backtrack
-    # otherwise, keep searching
-
     print("Not found yet!")
-    break
+    print(path)
+    # break
